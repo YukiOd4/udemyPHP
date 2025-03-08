838928c96f5dfec8d16dd181d6ee207fa043a6ca
yuki <odajpyuki2@gmail.com>
2025-03-08 19:54:52 -0300

    atualizacao

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'umadminaleatorio/composers',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f21bf7eee3b0916e4c41c2199a17af96c384de98',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'umadminaleatorio/composers' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f21bf7eee3b0916e4c41c2199a17af96c384de98',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
